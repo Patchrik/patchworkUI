ddf863af55013fd043548666cdf0127dcb3599f3
Patchrik <Pklly92@gmail.com>
2024-02-06 19:16:49 -0600

    converting to TS, hopefully this worked?

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+// Import your components here
+// import Button from './Button';
+// import Card from './Card';
+// import Modal from './Modal';
+
+// Export your components here
+// export { Button, Card, Modal };
